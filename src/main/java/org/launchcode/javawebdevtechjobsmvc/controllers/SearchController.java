39092a30d0e1a732abf469014d2d7700a8583d39
sbasu1 <ghoshsreya@gmail.com>
2020-01-30 08:47:15 -0600

    Assignment

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -1,14 +1,15 @@
 package org.launchcode.javawebdevtechjobsmvc.controllers;
 
-import org.launchcode.javawebdevtechjobsmvc.models.Job;
 import org.launchcode.javawebdevtechjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
+import static org.launchcode.javawebdevtechjobsmvc.models.JobData.findByColumnAndValue;
 
 /**
  * Created by LaunchCode
@@ -24,5 +25,17 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @RequestMapping(value = "results", method = {RequestMethod.GET, RequestMethod.POST})
+   // @ResponseBody
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
+        System.out.println(searchTerm);
+        System.out.println(searchType);
+        System.out.println(JobData.findByColumnAndValue(searchType, searchTerm));
+        model.addAttribute("columns", columnChoices);
+
+        model.addAttribute("searchResult", JobData.findByColumnAndValue(searchType, searchTerm));
+
+        return "search";
+    };
 
 }
